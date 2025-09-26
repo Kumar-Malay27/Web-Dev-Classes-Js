8b121c02f3a7ad4d1a14746eb8531c67ae79d283
Kumar Malay <kumarmalay290@gmail.com>
2025-09-26 16:26:45 +0530

    Day 1 Js

--- Day 1/index.js
+++ Day 1/index.js
@@ -1,8 +1,12 @@
-let name = "John";
-let age = 20;
-age = 30;
-console.log(name, age);
+// let name = "John";
+// let age = 20;
+// age = 30;
+// console.log(name, age);
 
-const x = 10;
-// x=20;
-console.log(x);
+// const x = 10;
+// // x=20;
+// console.log(x);
+
+var a = 10;
+a=20;
+console.log(a);

abeb2703cbc1eef8e597e1f99cacb55bc3d08413
Kumar Malay <kumarmalay290@gmail.com>
2025-09-29 20:11:22 +0530

    Day 3 Js

--- Day 3/index.js
+++ Day 3/index.js
@@ -1,43 +1,52 @@
-// Operators
-// Arithmetic Operators
-console.log(5 + 3); // Addition
-console.log(5 - 3); // Subtraction
-console.log(5 * 3); // Multiplication
-console.log(5 / 3); // Division
-console.log(5 % 3); // Modulus -> Remainder
-console.log(5 ** 3); // Exponentiation -> 5*5*5
+// // Operators
+// // Arithmetic Operators
+// console.log(5 + 3); // Addition
+// console.log(5 - 3); // Subtraction
+// console.log(5 * 3); // Multiplication
+// console.log(5 / 3); // Division
+// console.log(5 % 3); // Modulus -> Remainder
+// console.log(5 ** 3); // Exponentiation -> 5*5*5
 
-// Assignment Operators
-let x = 20;
-let y = 10;
-// shorthand assignment
-x += y; // x = x + y
-x -= y; // x = x - y
-x *= y; // x = x * y
-x /= y; // x = x / y
-x %= y; // x = x % y
-x **= y; // x = x ** y
-console.log(x);
+// // Assignment Operators
+// let x = 20;
+// let y = 10;
+// // shorthand assignment
+// x += y; // x = x + y
+// x -= y; // x = x - y
+// x *= y; // x = x * y
+// x /= y; // x = x / y
+// x %= y; // x = x % y
+// x **= y; // x = x ** y
+// console.log(x);
 
-// Comparison Operators
-console.log(x > y);
-console.log(x < y);
-console.log(x >= y);
-console.log(x <= y);
-console.log(x == y);
-console.log(x != y);
-console.log(x === y); // first check TYPE then compare VALUE
-console.log(x !== y); // first check TYPE then compare VALUE
+// // Comparison Operators
+// console.log(x > y);
+// console.log(x < y);
+// console.log(x >= y);
+// console.log(x <= y);
+// console.log(x == y);
+// console.log(x != y);
+// console.log(x === y); // first check TYPE then compare VALUE
+// console.log(x !== y); // first check TYPE then compare VALUE
 
-// NaN (Not a Number) -> type of NaN is number
-let a = "121ac";
-let b = Number(a);
-console.log(b);
-console.log(typeof b);
+// // NaN (Not a Number) -> type of NaN is number
+// let a = "121ac";
+// let b = Number(a);
+// console.log(b);
+// console.log(typeof b);
 
-// Number -> String
-let c = 10;
-let d = String(c);
-console.log(d);
-console.log(typeof d);
+// // Number -> String
+// let c = 10;
+// let d = String(c);
+// console.log(d);
+// console.log(typeof d);
+
+// // boolean -> number
+// console.log(Number(true)); // 1
+// console.log(Number(false)); // 0
+
+// // null -> number
+// console.log(Number(null)); // 0
+// // undefined -> number
+// console.log(Number(undefined)); // NaN
 

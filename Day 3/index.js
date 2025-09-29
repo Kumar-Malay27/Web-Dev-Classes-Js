183725f28c5d3c86253b71d6085822d398c2cc8f
Kumar Malay <kumarmalay290@gmail.com>
2025-09-29 18:34:25 +0530

    Day 3 Js

--- Day 3/index.js
+++ Day 3/index.js
@@ -6,3 +6,16 @@ console.log(5 * 3); // Multiplication
 console.log(5 / 3); // Division
 console.log(5 % 3); // Modulus -> Remainder
 console.log(5 ** 3); // Exponentiation -> 5*5*5
+
+// Assignment Operators
+let x = 20;
+let y = 10;
+// shorthand assignment
+x += y; // x = x + y
+x -= y; // x = x - y
+x *= y; // x = x * y
+x /= y; // x = x / y
+x %= y; // x = x % y
+x **= y; // x = x ** y
+console.log(x);
+

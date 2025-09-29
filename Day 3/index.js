810d92c1073ed0f84a9bddbd6ae59d0a20feb229
Kumar Malay <kumarmalay290@gmail.com>
2025-09-29 18:28:52 +0530

    Day 3 Js

---
+++ Day 3/index.js
@@ -0,0 +1,8 @@
+// Operators
+// Arithmetic Operators
+console.log(5 + 3); // Addition
+console.log(5 - 3); // Subtraction
+console.log(5 * 3); // Multiplication
+console.log(5 / 3); // Division
+console.log(5 % 3); // Modulus -> Remainder
+console.log(5 ** 3); // Exponentiation -> 5*5*5

c949c9a4f5e52d57caec576cd2cab5d44382beed
Kumar Malay <kumarmalay290@gmail.com>
2025-09-30 20:48:26 +0530

    console.log(Math);

---
+++ Day 4/math.js
@@ -0,0 +1,10 @@
+console.log(Math.abs(-19));
+console.log(Math.PI);
+console.log(Math.LN10);
+console.log(Math.SQRT2);
+console.log(Math.ceil(7.5));
+console.log(Math.floor(7.5));
+console.log(Math.log10(20));
+console.log(Math.max(23,324,54,4,854,83));
+// Math.random() generates random values between[0,1) where 0 is included but 1 is not.
+console.log(Math.random());

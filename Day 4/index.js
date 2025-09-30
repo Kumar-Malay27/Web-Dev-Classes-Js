299132ecd5f549c62e2a61171a9ac5b8bc7b848d
Kumar Malay <kumarmalay290@gmail.com>
2025-09-30 20:10:51 +0530

    Day 4 Js

--- Day 4/index.js
+++ Day 4/index.js
@@ -1,5 +1,15 @@
 // Number
-let a = 10;
-let b = 345.6821; // number doesn't change beacuse it is primitive and primitive data types are immutable
-console.log(b.toFixed(1)); // returns string
-console.log(b.toPrecision(4)) // returns string
+// let a = 10;
+// let b = 345.6821; // number doesn't change beacuse it is primitive and primitive data types are immutable
+// console.log(b.toFixed(1)); // returns string
+// console.log(b.toPrecision(4)) // returns string
+
+// let a = new Number(20); // returns object
+// let b = new Number(20); // returns object
+
+// console.log(a==b); // false because it creates different reference and they repsresent different values.
+
+// let a = new Number(20);
+// let b = a;
+
+// console.log(a==b); // true beacuse both the references represents same values.

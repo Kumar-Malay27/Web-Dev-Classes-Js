05412910bd17bed65d4250eb69a24447ca6d2204
Kumar Malay <kumarmalay290@gmail.com>
2025-09-30 19:56:49 +0530

    Day 4 Js

--- Day 4/index.js
+++ Day 4/index.js
@@ -1,4 +1,5 @@
 // Number
 let a = 10;
-let b = 345.6821;
-console.log(b);
+let b = 345.6821; // number doesn't change beacuse it is primitive and primitive data types are immutable
+console.log(b.toFixed(1)); // returns string
+console.log(b.toPrecision(4)) // returns string

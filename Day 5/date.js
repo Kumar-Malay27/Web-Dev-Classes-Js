7547651bc002bc5e49cddfeff0f1b4656091a6b1
Kumar Malay <kumarmalay290@gmail.com>
2025-10-01 23:41:49 +0530

    Day 5 Js

--- Day 5/date.js
+++ Day 5/date.js
@@ -9,4 +9,9 @@
 // console.log(now.getFullYear());
 // console.log(now.getMonth());
 
+// timestamp
 const now = Date.now();
+const dates = new Date(-12312312);
+
+console.log(dates.toString());
+console.log(now);

7f1eff63d430f4969742989298cea00746d6a2e4
Kumar Malay <kumarmalay290@gmail.com>
2025-10-01 23:18:26 +0530

    Day 5 Js

---
+++ Day 5/date.js
@@ -0,0 +1,10 @@
+// Date
+const now = new Date();
+// console.log(now.toString());
+// console.log(now.toISOString());
+// console.log(now.toLocaleString());
+
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getFullYear());
+console.log(now.getMonth());

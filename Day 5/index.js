f9db473321c35c2eeb2c74030213ffe0e6c43b01
Kumar Malay <kumarmalay290@gmail.com>
2025-10-01 20:07:45 +0530

    Day 5 Js

--- Day 5/index.js
+++ Day 5/index.js
@@ -5,11 +5,16 @@
 // const str3 = `Today is ${date}st of October.`;
 // console.log(str1, str2, str3);
 
-const str = `How are you doing?`;
+// const str = `How are you doing?`;
 // console.log(str.length);
 
 // str[2] = "s";
 // console.log(str); //doesn't change anything because string are immutable.
 
 // Uppercase
-console.log(str.toUpperCase())
+const str = `How are you doing are?`;
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+console.log(str.indexOf('are'));
+console.log(str.lastIndexOf('are'));
+console.log(str.includes('are'));

26fd115fb49ce00f2d6714333cc2b51974918c7d
Kumar Malay <kumarmalay290@gmail.com>
2025-10-01 19:42:46 +0530

    Day 5 Js

--- Day 5/index.js
+++ Day 5/index.js
@@ -1,8 +1,6 @@
 // String
 const str1 = "Hello";
 const str2 = "World";
-const str3 = `How are 
-you 
-doing?
-`;
+const date = 1
+const str3 = `Today is ${date}st of October.`;
 console.log(str1, str2, str3);

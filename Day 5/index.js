a1aac5cfc2d486ed7e6b777f556c4a168207eb28
Kumar Malay <kumarmalay290@gmail.com>
2025-10-01 20:25:39 +0530

    Day 5 Js

--- Day 5/index.js
+++ Day 5/index.js
@@ -12,10 +12,22 @@
 // console.log(str); //doesn't change anything because string are immutable.
 
 // Uppercase
-const str = `How are you doing are?`;
+// const str = `How are you doing are?`;
 // console.log(str.toUpperCase());
 // console.log(str.toLowerCase());
 // console.log(str.indexOf('are'));
 // console.log(str.lastIndexOf('are'));
 // console.log(str.includes('are'));
 
+// slice
+// console.log(str.slice(2,7));
+// console.log(str.slice(3));
+// console.log(str.slice(-4));
+
+// const str = `How are you doing are?`;
+// console.log(str.substring(2,5));
+
+const a = "Kumar";
+const b = "Malay";
+const c = a + " " + b;
+console.log(c);

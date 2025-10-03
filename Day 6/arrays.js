544c1f49a68be15e2ffc8b81d057ef33353894e2
Kumar Malay <kumarmalay290@gmail.com>
2025-10-03 18:49:45 +0530

    Day 6 Js

---
+++ Day 6/arrays.js
@@ -0,0 +1,17 @@
+// Array
+
+let marks = [100,50,70,80,90];
+console.log(marks);
+console.log(marks.length);
+let arr = [100,30,"Rohit",true];
+// console.log(arr[2]);
+// console.log(typeof arr);
+
+// arr[1] = 90;
+// console.log(arr);
+
+// Push
+arr.push(90);
+arr.push("Hello");
+
+console.log(arr);

1af2cab6e3c0f149eca8a2af829d77cb1a9d38c2
Kumar Malay <kumarmalay290@gmail.com>
2025-10-03 19:06:54 +0530

    Day 6 Js

--- Day 6/arrays.js
+++ Day 6/arrays.js
@@ -1,9 +1,9 @@
 // Array
 
-let marks = [100,50,70,80,90];
-console.log(marks);
-console.log(marks.length);
-let arr = [100,30,"Rohit",true];
+// let marks = [100,50,70,80,90];
+// console.log(marks);
+// console.log(marks.length);
+// let arr = [100,30,"Rohit",true];
 // console.log(arr[2]);
 // console.log(typeof arr);
 
@@ -11,22 +11,23 @@ let arr = [100,30,"Rohit",true];
 // console.log(arr);
 
 // Push: It inserts elements at end.
-arr.push(90);
-arr.push("Hello");
+// arr.push(90);
+// arr.push("Hello");
 
-console.log(arr);
+// console.log(arr);
 
 // Pop: It deletes element from end.
-arr.pop();
-console.log(arr);
+// arr.pop();
+// console.log(arr);
 
 // Add elements at first -> unshift()
 
-arr.unshift(15);
-arr.unshift(30);
-console.log(arr);
+// arr.unshift(15);
+// arr.unshift(30);
+// console.log(arr);
 
 // Delete elements at first -> shift()
 
-arr.shift();
-console.log(arr);
+// arr.shift();
+// console.log(arr);
+

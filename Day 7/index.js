2233afbff94be818167ad82f77b55137695543f2
Kumar Malay <kumarmalay290@gmail.com>
2025-10-06 18:43:17 +0530

    Day 7 Js

--- Day 7/index.js
+++ Day 7/index.js
@@ -1,10 +1,15 @@
 // Object
-
+// CRUD Operation: Create Read Update delete
+// Create
 const user = {
     name: "Malay",
     age: 20,
     emailId: "malay290@gmail.com",
     amount: 65000
 }
+// Read
 console.log(user); 
 console.log(user.emailId);
+// Update
+user.aadhar = 1234;
+user.amount = 50000;

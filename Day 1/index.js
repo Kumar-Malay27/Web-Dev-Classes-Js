f40a44b766840dac7b3016a41348a5fa06331093
Kumar Malay <kumarmalay290@gmail.com>
2025-10-12 17:11:40 +0530

    Day 1 Js

--- Day 1/index.js
+++ Day 1/index.js
@@ -11,45 +11,57 @@
 // a=20;
 // console.log(a);
 
+// if (true){
+//     var a = 20;
+// }
+
+// console.log(a);
+
+// function fun(){
+//     var c = 20;
+// }
+
+// console.log(c);
+
 // number
-let num1 = 10;
-let num2 = 20.5;
-console.log(num1, num2);
+// let num1 = 10;
+// let num2 = 20.5;
+// console.log(num1, num2);
 
 // string
-let c="Hello";
-let d='Hello World';
-console.log(c, d);
+// let c="Hello";
+// let d='Hello World';
+// console.log(c, d);
 
 // boolean
-let isTrue = true;
-let isFalse = false;
-console.log(isTrue, isFalse);
+// let isTrue = true;
+// let isFalse = false;
+// console.log(isTrue, isFalse);
 
 // undefined
-let und;
-console.log(und);
+// let und;
+// console.log(und);
 
 // bigint
-let bigIntNum = 123456789012345678901234567890n;
-console.log(bigIntNum);
+// let bigIntNum = 123456789012345678901234567890n;
+// console.log(bigIntNum);
 
 // null
-let n = null;
-console.log(n);
+// let n = null;
+// console.log(n);
 
 // symbol
-const id1 = Symbol('id');
-console.log(id1);
+// const id1 = Symbol('id');
+// console.log(id1);
 
 // array
-let arr = [1, 2, 3, 4, 5, "Hello", true];
-console.log(arr);
+// let arr = [1, 2, 3, 4, 5, "Hello", true];
+// console.log(arr);
 
 // object
-let user = {
-    name: "John",
-    age: 30,
-    account: 12323,
-    category: "gen",
-};
+// let user = {
+    // name: "John",
+    // age: 30,
+    // account: 12323,
+    // category: "gen",
+// };

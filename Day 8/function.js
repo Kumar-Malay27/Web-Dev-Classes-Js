dba0f7147870864ab0eea066cf2fffb4b6c33b82
Kumar Malay <kumarmalay290@gmail.com>
2025-10-18 13:48:36 +0530

    Day 8 Js

--- Day 8/function.js
+++ Day 8/function.js
@@ -1,16 +1,28 @@
 // function
 
-function greeting() {
-    console.log("Hello World!");
-}
+// function greeting() {
+//     console.log("Hello World!");
+// }
 
-function addNumbers(n1, n2, n3=0, n4=0) {
-    const sum = n1 + n2 + n3 + n4;
-    console.log(sum);
-}
+// function addNumbers(n1, n2, n3=0, n4=0) {
+//     const sum = n1 + n2 + n3 + n4;
+//     console.log(sum);
+// }
 
 // greeting();
 // addNumbers(5, 10);
-addNumbers(54, 10);
-addNumbers(1, 2, 3);
-addNumbers(5, 10, 15, 20);
+
+// rest operator
+
+function addNumbers(...num) {
+    let sum = 0;
+    for (let n of num) {
+        sum += n;
+    }
+    console.log(sum);
+}
+
+addNumbers(6, 7);
+addNumbers(6, 7, 8);
+addNumbers(6, 7, 8, 9);
+addNumbers(7, 8, 12, 12, 11, 12, 41, 12);

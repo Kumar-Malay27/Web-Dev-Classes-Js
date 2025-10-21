dd2d881a36c68e033e6ebf93f9055f75ee36949b
Kumar Malay <kumarmalay290@gmail.com>
2025-10-21 14:27:47 +0530

    Day 8 Js

--- Day 8/callback.js
+++ Day 8/callback.js
@@ -14,3 +14,21 @@
 
 // meet(greet);
 // meet(dance);
+
+
+function blinkitOrderPlaced(){
+    console.log("We have started packing your Order");
+}
+
+function zomatoOrderPlaced(){
+    console.log("We have started preparing your food");
+}
+
+function payment(amount,callback){
+    console.log(`${amount} payment has initilized`)
+    console.log("Payment is received");
+    callback();
+}
+
+payment(500,zomatoOrderPlaced);
+payment(300,blinkitOrderPlaced);

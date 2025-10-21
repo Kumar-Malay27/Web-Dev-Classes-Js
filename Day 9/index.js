df0cc2813d25157f27312e1adf048e6af99ab5c3
Kumar Malay <kumarmalay290@gmail.com>
2025-10-21 18:32:30 +0530

    Day 9 Js

---
+++ Day 9/index.js
@@ -0,0 +1,16 @@
+// Execution Context
+// Memory Allocation Phase
+// Execution Phase
+
+// console.log(a);
+// var a = 10;
+// var b = 20;
+
+// function addNumber(num1, num2) {
+//     var sum = num1 + num2;
+//     return sum;
+// }
+
+// var sumResult1 = addNumber(a, b);
+// var sumResult2 = addNumber(4, 5);
+// console.log(sumResult1, sumResult2);

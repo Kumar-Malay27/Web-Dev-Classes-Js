33d08948ecc695ecd1f135ffbe68ad7079bd8e05
Kumar Malay <kumarmalay290@gmail.com>
2025-10-21 14:14:49 +0530

    Day 8 Js

--- Day 8/callback.js
+++ Day 8/callback.js
@@ -1,16 +1,16 @@
-function greet(){
-    console.log("Hello how are you?");
-}
+// function greet(){
+//     console.log("Hello how are you?");
+// }
 
-function dance(){
-    console.log("Dancing...");
-}
+// function dance(){
+//     console.log("Dancing...");
+// }
 
-function meet(callback){
-    console.log("I am going to meet someone!");
-    callback();
-    console.log("Meeting is over.");
-}
+// function meet(callback){
+//     console.log("I am going to meet someone!");
+//     callback();
+//     console.log("Meeting is over.");
+// }
 
-meet(greet);
-meet(dance);
+// meet(greet);
+// meet(dance);

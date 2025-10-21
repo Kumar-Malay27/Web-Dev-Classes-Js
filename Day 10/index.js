73b6ef26f9507cd3bdb01184fdf71c2bd5500dfa
Kumar Malay <kumarmalay290@gmail.com>
2025-10-21 22:47:06 +0530

    Day 10 Js

--- Day 10/index.js
+++ Day 10/index.js
@@ -1,12 +1,16 @@
 // scope and closure , HOF
+// scope : block scope, function scope, global scope
 
+// global scope
 let a = 10;
 const b = 20;
 
+// block scope
 if (true) {
     console.log(b);
 }
 
+// function scope
 function greet() {
     console.log(a);
 }
